4f34629428402d7febe50f840bb7099d9409d946
Jason R <58869791+jason-rivera@users.noreply.github.com>
2021-02-28 19:11:12 -0800

    Add files via upload

---
+++ typing.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+  // typing text animation script
+  var typed = new Typed(".typing", {
+      strings: [
+        "Software Developer",
+        "Database Developer",
+        "Full-stack Developer"
+      ],
+      typeSpeed: 40,
+      backSpeed: 30,
+      loop: true
+  });
+
+
+});
+
